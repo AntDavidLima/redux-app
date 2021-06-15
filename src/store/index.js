58a63d7ef9cf68e6ab7e18a23db98063e2c9d3d2
David Lima <antdavidlima@gmail.com>
2021-06-15 18:28:08 -0300

    configured redux saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,21 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools()
+  composeWithDevTools(
+    applyMiddleware(...middlewares)
+  )
 );
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
---
+++ src/store/modules/cart/sagas.js
@@ -0,0 +1,15 @@
+import { all, takeLatest, select } from 'redux-saga/effects';
+
+function* checkProductStock({payload}) {
+  const { product } = payload;
+
+  const currentQuantity = yield select(state => {
+    return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0;
+  });
+
+  console.log(currentQuantity);
+}
+
+export default all([
+  takeLatest('ADD_PRODUCT_TO_CART', checkProductStock)
+]);
