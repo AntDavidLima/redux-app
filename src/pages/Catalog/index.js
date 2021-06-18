2c5be6859961fe04a45a06291c65492c8dbd174c
David Lima <antdavidlima@gmail.com>
2021-06-17 23:19:27 -0300

    add logout funcationality

--- src/pages/Catalog/index.js
+++ src/pages/Catalog/index.js
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
 
 import './style.css';
 
 import api from '../../services/api';
+import { logout } from '../../services/auth';
 
 import CatalogItem from '../../components/CatalogItem';
 import Cart from '../../components/Cart';
@@ -28,6 +31,11 @@ function Catalog() {
             Produtos
           </Typography>
           <Cart />
+          <Link>
+            <Button onClick={() => logout()}>
+              Logout
+            </Button>
+          </Link>
         </Toolbar>
       </AppBar>
       <br />
